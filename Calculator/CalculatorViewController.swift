539351be4b9ecceca5afcb2ec825df180a792a36
Shaurya <shaurya50211@gmail.com>
2022-08-22 09:45:29 -0400

    Made app code more reorganized

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -12,31 +12,35 @@ class CalculatorViewController: UIViewController {
     
     private var isFinishedTyping = true
     
+    private var displayValue: Double {
+        get {
+            guard let displayText = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display.text to Double!")
+            }
+            return displayText
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         isFinishedTyping = true
         
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Cannot convert display label text to a Double")
-        }
-        
         if let calcMethod = sender.currentTitle {
             if calcMethod == "+/-" {
-                displayLabel.text = String(number * -1)
+                displayValue *= -1
             } else if calcMethod == "AC" {
                 displayLabel.text = "0"
             } else if calcMethod == "%" {
-                displayLabel.text = String(number / 100)
+                displayValue *= 0.01
             }
         }
     }
     
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        //
-        //        sender.backgroundColor = RandomFlatColor()
-        //        sender.titleLabel?.textColor = ContrastColorOf((sender.backgroundColor ?? HexColor("1D9BF6"))!, returnFlat: true)
         
-        // then start the animation
         UIView.animate(withDuration: 0.15) {
             sender.backgroundColor = RandomFlatColor()
             sender.titleLabel?.textColor = ContrastColorOf((sender.backgroundColor ?? HexColor("1D9BF6"))!, returnFlat: true)
@@ -47,8 +51,6 @@ class CalculatorViewController: UIViewController {
             }
         }
         
-        
-        //What should happen when a number is entered into the keypad
         if let numVal = sender.currentTitle{
             
             if isFinishedTyping {
@@ -56,31 +58,26 @@ class CalculatorViewController: UIViewController {
                 isFinishedTyping = false
             } else {
                 if displayLabel.text!.range(of:".") != nil {
-                    if numVal != "."{ // Check wether the user wants to enter another .
+                    if numVal != "." {
                         displayLabel.text = displayLabel.text! + numVal
-                    } else { // if so, return
+                    } else {
                         return
                     }
                 } else {
-                    // if the range of "." is nil, just enter the value without any check on "."
                     displayLabel.text = displayLabel.text! + numVal
                 }
                 
-                //What should happen when a non-number button is pressed
-                isFinishedTyping = true
+                isFinishedTyping = false
                 
                 if let calcMethod = sender.currentTitle {
                     
                     if calcMethod == "AC"{
                         displayLabel.text = "0"
                     } else {
-                        guard let number = Double(displayLabel.text!) else {
-                            fatalError("Error converting displaylabel.text to a Double!")
-                        }
                         if calcMethod == "+/-"{
-                            displayLabel.text = String(number * -1)
+                            displayLabel.text = String(displayValue * -1)
                         } else if calcMethod == "%"{
-                            displayLabel.text = String(number / 100)
+                            displayLabel.text = String(displayValue / 100)
                         }
                     }
                 }
